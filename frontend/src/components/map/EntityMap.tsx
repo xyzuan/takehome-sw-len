4519b12d9a532178e444fccb0dc13a7dd615a54d
xyzuan <jodyyuan@xyzuan.com>
2026-09-18 17:03:20 +0000

    ui: set bbox debounce to 100ms (safe default)

--- frontend/src/components/map/EntityMap.tsx
+++ frontend/src/components/map/EntityMap.tsx
@@ -25,7 +25,7 @@ function BboxTracker() {
       if (timer) clearTimeout(timer);
       timer = setTimeout(() => {
         setBbox(computeBbox());
-      }, 50);
+      }, 100);
     };
 
     if (isLoaded) {
